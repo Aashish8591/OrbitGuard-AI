1010eddda08d90aa38fb750728c8ba396924e758
Aashish Patil <aashishpatil2142004@gmail.com>
2026-07-17 11:27:05 +0530

    Initial project setup

---
+++ orbitguard-server/src/main/java/com/orbitguard/OrbitguardServerApplication.java
@@ -0,0 +1,13 @@
+package com.orbitguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitguardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitguardServerApplication.class, args);
+	}
+
+}
---
+++ orbitguard-server/src/test/java/com/orbitguard/OrbitguardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbitguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitguardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
